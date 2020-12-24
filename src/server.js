33e999fd3f3173caa723e03391c499dfd0062af2
LR <lehorichard@gmail.com>
2020-12-24 19:42:34 +0100

    build: deploy client

--- src/server.js
+++ src/server.js
@@ -24,6 +24,7 @@ app.use(bodyParser.urlencoded({
 }))
 app.use(bodyParser.json({ limit: '50mb' }))
 app.use(express.json({limit: '50mb'}))
+app.use(express.static('build'))
 mongoose.set('useFindAndModify', false)
 app.use(cors())
 app.options('*', cors())
